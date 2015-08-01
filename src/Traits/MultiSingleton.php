81caa2f980e09f01d76baa463e52ac382814d636
Shea Lewis <shea.lewis89@gmail.com>
2015-08-01 15:06:50 -0700

    Added multisingleton trait

---
+++ src/Traits/MultiSingleton.php
@@ -0,0 +1,56 @@
+<?php
+namespace Caffeinated\Beverage\Traits;
+
+/**
+ * Multi-Singleton Trait
+ *
+ * @author    Caffeinated Dev Team
+ * @copyright Copyright (c) 2015, Caffeinated
+ * @license   https://tldrlegal.com/license/mit-license MIT License
+ * @package   Caffeinated\Beverage
+ */
+trait MultiSingleton
+{
+    /**
+     * @var array
+     */
+    private static $instances = array();
+
+    /**
+     * Create a new multisingleton instance.
+     *
+     * @return void
+     */
+    protected function __construct()
+    {
+        if (method_exists($this, 'init')) {
+            call_user_func_array([$this, 'init'], func_get_args());
+        }
+    }
+
+    /**
+     * Initialize a new instance.
+     *
+     * @return mixed
+     */
+    abstract protected function init();
+
+    /**
+     * Get the current instance.
+     *
+     * @param  string  $name
+     * @return static
+     */
+    public static function getInstance($name = 'default')
+    {
+        if (isset(static::$instances[$name])) {
+            return static::$instance[$name];
+        }
+
+        $instance = new static();
+
+        static::$instances[$name] = $instance;
+
+        return $instance;
+    }
+}
