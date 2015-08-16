73a6c615ed7282a478c43ba14bd1119f434c92b5
Robin Radic <robin@radic.nl>
2015-08-16 05:40:45 +0200

    removed validation stuff, needs different implementation

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -49,4 +49,114 @@ class Dotenv extends \Dotenv
         }
         return $vars;
     }
+
+    /**
+     * Takes value as passed in by developer and:
+     * - ensures we're dealing with a separate name and value, breaking apart the name string if needed
+     * - cleaning the value of quotes
+     * - cleaning the name of quotes
+     * - resolving nested variables
+     *
+     * @param $name
+     * @param $value
+     * @return array
+     */
+    private static function normaliseEnvironmentVariable($name, $value)
+    {
+        list($name, $value) = self::splitCompoundStringIntoParts($name, $value);
+        $name  = self::sanitiseVariableName($name);
+        $value = self::sanitiseVariableValue($value);
+        $value = self::resolveNestedVariables($value);
+
+        return array($name, $value);
+    }
+
+    /**
+     * If the $name contains an = sign, then we split it into 2 parts, a name & value
+     *
+     * @param $name
+     * @param $value
+     * @return array
+     */
+    private static function splitCompoundStringIntoParts($name, $value)
+    {
+        if (strpos($name, '=') !== false) {
+            list($name, $value) = array_map('trim', explode('=', $name, 2));
+        }
+
+        return array($name, $value);
+    }
+
+    /**
+     * Strips quotes from the environment variable value.
+     *
+     * @param $value
+     * @return string
+     */
+    private static function sanitiseVariableValue($value)
+    {
+        $value = trim($value);
+        if (!$value) return '';
+        if (strpbrk($value[0], '"\'') !== false) { // value starts with a quote
+            $quote = $value[0];
+            $regexPattern = sprintf('/^
+                %1$s          # match a quote at the start of the value
+                (             # capturing sub-pattern used
+                 (?:          # we do not need to capture this
+                  [^%1$s\\\\] # any character other than a quote or backslash
+                  |\\\\\\\\   # or two backslashes together
+                  |\\\\%1$s   # or an escaped quote e.g \"
+                 )*           # as many characters that match the previous rules
+                )             # end of the capturing sub-pattern
+                %1$s          # and the closing quote
+                .*$           # and discard any string after the closing quote
+                /mx', $quote);
+            $value = preg_replace($regexPattern, '$1', $value);
+            $value = str_replace("\\$quote", $quote, $value);
+            $value = str_replace('\\\\', '\\', $value);
+        } else {
+            $parts = explode(' #', $value, 2);
+            $value = $parts[0];
+        }
+        return trim($value);
+    }
+
+    /**
+     * Strips quotes and the optional leading "export " from the environment variable name.
+     *
+     * @param $name
+     * @return string
+     */
+    private static function sanitiseVariableName($name)
+    {
+        return trim(str_replace(array('export ', '\'', '"'), '', $name));
+    }
+
+    /**
+     * Look for {$varname} patterns in the variable value and replace with an existing
+     * environment variable.
+     *
+     * @param $value
+     * @return mixed
+     */
+    private static function resolveNestedVariables($value)
+    {
+        if (strpos($value, '$') !== false) {
+            $value = preg_replace_callback(
+                '/{\$([a-zA-Z0-9_]+)}/',
+                function ($matchedPatterns) {
+                    $nestedVariable = Dotenv::findEnvironmentVariable($matchedPatterns[1]);
+                    if (is_null($nestedVariable)) {
+                        return $matchedPatterns[0];
+                    } else {
+                        return  $nestedVariable;
+                    }
+                },
+                $value
+            );
+        }
+
+        return $value;
+    }
+
 }
