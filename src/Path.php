19b1b2aec18739f3b5352abc6bef18ccc5569619
Robin Radic <robin@radic.nl>
2015-08-16 20:16:55 +0200

    added getDirectoryName function to Path

--- src/Path.php
+++ src/Path.php
@@ -16,25 +16,41 @@ class Path extends BasePath
     /**
      * Joins a split file system path.
      *
-     * @param  array|string  $path
+     * @param  array|string $path
      * @return string
      */
     public static function join()
     {
         $arguments = func_get_args();
 
-        if (func_get_args() === 1 and is_array($arguments[0])) {
-            $arguments = $arguments[0];
+        if ( func_get_args() === 1 and is_array($arguments[ 0 ]) )
+        {
+            $arguments = $arguments[ 0 ];
         }
 
-        foreach ($arguments as $key => $argument) {
-            $arguments[$key] = Str::removeRight($arguments[$key], '/');
+        foreach ( $arguments as $key => $argument )
+        {
+            $arguments[ $key ] = Str::removeRight($arguments[ $key ], '/');
 
-            if ($key > 0) {
-                $arguments[$key] = Str::removeLeft($arguments[$key], '/');
+            if ( $key > 0 )
+            {
+                $arguments[ $key ] = Str::removeLeft($arguments[ $key ], '/');
             }
         }
 
         return implode(DIRECTORY_SEPARATOR, $arguments);
+    }
+
+    public static function getDirectoryName($path)
+    {
+        $path = realpath($path);
+        if ( is_dir($path) )
+        {
+            return last(explode(DIRECTORY_SEPARATOR, $path));
+        }
+        else
+        {
+            return static::getDirectory($path);
+        }
     }
 }
