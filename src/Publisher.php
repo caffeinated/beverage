30e8b627d46bfc7f39f7b9db3292052e2b55224b
Robin Radic <robin@radic.nl>
2015-08-31 17:36:35 +0200

    added base publisher class, requires more work

---
+++ src/Publisher.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Part of the Caffeinated PHP packages.
+ *
+ * MIT License and copyright information bundled with this package in the LICENSE file
+ */
+namespace Caffeinated\Beverage;
+
+use Illuminate\Container\Container;
+
+/**
+ * This is the Publisher.
+ *
+ * @package        Caffeinated\Beverage
+ * @author         Caffeinated Dev Team
+ * @copyright      Copyright (c) 2015, Caffeinated
+ * @license        https://tldrlegal.com/license/mit-license MIT License
+ */
+class Publisher
+{
+    /**
+     * The package name (ex: caffeinated/themes)
+     * @var string
+     */
+    protected $package;
+
+    /**
+     * The path to the source files which are to be published
+     * @var string
+     */
+    protected $sourcePath;
+
+    /** @var \Illuminate\Filesystem\Filesystem */
+    protected $files;
+
+    protected $destinationPath;
+
+    /**
+     * @param \Caffeinated\Beverage\Filesystem $files
+     */
+    protected function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+    }
+
+    /**
+     * publish
+     */
+    public function publish()
+    {
+        $destination = config_path('packages/' . $this->package);
+        if ( ! $this->files->exists($this->sourcePath) )
+        {
+            return;
+        }
+        if ( ! $this->files->exists($this->destinationPath) )
+        {
+            $this->files->makeDirectory($destination, 0755, true);
+        }
+        $this->files->copyDirectory($this->sourcePath, $destination);
+    }
+
+    /**
+     * Create a new publisher instance
+     *
+     * @param \Caffeinated\Beverage\Filesystem $files
+     * @return static
+     */
+    public static function create(Filesystem $files)
+    {
+        return Container::getInstance()->make(static::class);
+    }
+
+    /**
+     * package
+     *
+     * @param $package
+     * @return $this
+     */
+    public function to($destinationPath)
+    {
+        $this->destinationPath = $destinationPath;
+
+        return $this;
+    }
+
+    /**
+     * from
+     *
+     * @param $sourcePath
+     * @return $this
+     */
+    public function from($sourcePath)
+    {
+        $this->sourcePath = $sourcePath;
+
+        return $this;
+    }
+}
